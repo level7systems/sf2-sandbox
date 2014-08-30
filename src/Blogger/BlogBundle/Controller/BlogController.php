c1ad298d855376a95bf1e88517531b259f8bdbb1
Chris Maciejewski <git@ssl7.net>
2014-08-30 15:22:38 +0100

    part 3 in progress...

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+        
+        if (!$blog) {
+            throw $this->createNotFoundException('Blog post not found');
+        }
+        
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog' => $blog
+        ));
+    }
+}
