2f5b791d9c5aac564cdf5080288012cb8248d43d
Chris Maciejewski <git@ssl7.net>
2014-08-30 15:55:31 +0100

    Part 3 completed

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -7,9 +7,24 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="blog")
+ * @ORM\HasLifecycleCallbacks
  */
 class Blog
 {
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+    
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedValue()
+    {
+       $this->setUpdated(new \DateTime());
+    }
+    
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
